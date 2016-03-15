eb676ee02e048c888d0bb7dcbfa98101a1ec729c
Jordan Meier and Mary Warrington <jbirdmeier@gmail.com>
2016-03-15 14:22:20 -0700

    Add conversion from K to F and display it on weather-interface

---
+++ js/temperature.js
@@ -0,0 +1,5 @@
+
+
+exports.tempConvert = function(temp) {
+  return ((temp-273.15)*9/5)+32;
+}
--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,12 +1,16 @@
 
-var apiKey = "ca7756173fb318fde21cd09003497fed";
+var apiKey = require('./../.env').apiKey;
+var tempConvert = require('./../js/temperature.js').tempConvert;
+
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%. The temperature in " + city + " is " + Math.floor(tempConvert(response.main.temp)) + " degrees F.");
+    }).fail(function(error) {
+      $('.showWeather').text(error.message);
     });
   });
 });
