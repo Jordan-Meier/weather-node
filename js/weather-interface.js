c510a06ef10f13588cbfd1f166324df241661815
Jordan Meier and Mary Warrington <marywarrington@gmail.com>
2016-03-15 14:45:57 -0700

    add a lot of hides and toggles to switch between showing fahrenheit and celsius

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,16 +1,43 @@
 
 var apiKey = require('./../.env').apiKey;
-var tempConvert = require('./../js/temperature.js').tempConvert;
+var tempConvertToF = require('./../js/temperature.js').tempConvertToF;
+var tempConvertToC = require('./../js/temperature.js').tempConvertToC;
 
 
 $(document).ready(function() {
+  $('.showWeather').hide();
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%. The temperature in " + city + " is " + Math.floor(tempConvert(response.main.temp)) + " degrees F.");
+      $('.showWeather').show();
+      $('#city').text(city);
+      $('#humidity').text(response.main.humidity);
+
+      $('#celsiusTemp').text(Math.floor(tempConvertToC(response.main.temp)));
+      $('#celsius').hide();
+      $('#switchToF').hide();
+
+      $('#fTemp').text(Math.floor(tempConvertToF(response.main.temp)));
+
+      $('#switchToC').click(function() {
+        $('#celsius').toggle();
+        $('#switchToF').toggle();
+        $('#f').toggle();
+        $('#switchToC').toggle();
+      });
+
+      $('#switchToF').click(function() {
+        $('#celsius').toggle();
+        $('#switchToF').toggle();
+        $('#f').toggle();
+        $('#switchToC').toggle();
+      });
+
     }).fail(function(error) {
       $('.showWeather').text(error.message);
     });
   });
 });
+
+// $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%. The temperature in " + city + " is " + Math.floor(tempConvertToF(response.main.temp)) + " degrees F.");
