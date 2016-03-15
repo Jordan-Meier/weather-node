de88728d06bf2ba580bba5aae890a3507bb5908e
Jordan Meier and Mary Warrington <jbirdmeier@gmail.com>
2016-03-15 14:01:00 -0700

    add index.html and weather-interface logic to display humidity

---
+++ js/weather-interface.js
@@ -0,0 +1,12 @@
+
+var apiKey = "ca7756173fb318fde21cd09003497fed";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
